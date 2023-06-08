33ccfbec4316bbe34ae406f9976e5578d5eed37a
skinchizer <mezo214@gmail.com>
2023-06-08 04:43:18 -0700

    s

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = len(sys.argv) - 1
+    sums = 0
+    for i in range(args):
+        sums += int(sys.argv[i + 1])
+    print("{}".format(sums))
