4bd5e63c4dc96cd40d41ba19978c2d8dde824c55
Mazen <mazen@Mazen.myguest.virtualbox.org>
2024-08-13 12:27:32 +0300

    done

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] % 2:
+            new_list[i] = False
+        else:
+            new_list[i] = True
+    return new_list
