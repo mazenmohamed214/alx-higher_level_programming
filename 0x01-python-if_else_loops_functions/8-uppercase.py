3c3a5677817bf253f5d07f336e89d9aedc173f5c
Mazen <mazen@Mazen.myguest.virtualbox.org>
2024-08-13 08:55:01 +0300

    s

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for i in str:
         if ord(i) in range(97, 123):
             i = chr(ord(i) - 32)
-        print(i, end="")
+        print("{}".format(i), end="")
     print("")
